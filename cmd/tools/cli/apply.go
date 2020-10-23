ba5b625028ab5ebf59b12aa509cd1e19279cd450
Harsimran Singh Maan <maan.harry@gmail.com>
2020-10-23 15:27:45 -0700

    parameterize plugin name

--- cmd/tools/cli/apply.go
+++ cmd/tools/cli/apply.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/kf5i/k3ai-core/internal/k8s/kctl"
@@ -9,16 +10,20 @@ import (
 )
 
 var applyCmd = &cobra.Command{
-	Use:   "apply",
+	Use:   "apply <plugin_name>",
 	Short: "Apply the plugin",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		if len(args) != 1 {
+			return errors.New("missing plugin name")
+		}
+		pluginName := args[0]
 		pluginList, err := plugins.GetPluginList()
 		if err != nil {
 			return err
 		}
 		fmt.Printf("Plugin list: %s\n", pluginList)
 
-		pluginSpecList, _ := plugins.GetPluginYamls("argo")
+		pluginSpecList, _ := plugins.GetPluginYamls(pluginName)
 		for _, pluginSpec := range *pluginSpecList {
 			fmt.Printf("Plugin YAML content: %s, name: %s \n", pluginSpec.Yaml, pluginSpec.PluginName)
 			kctl.Apply(pluginSpec, nil)
