kf5i/k3ai-core

commit f889a7540b1017a06893d212bffb0ba31a6fad2b

Change name space level (#37)
* Change name space level

* Change name space level

fixes: https://github.com/kf5i/k3ai-core/issues/36

* remove dependency

* cosmetics

* pr review

* pr review

internal/k8s/kctl/deploy.go
@@ -1,7 +1,9 @@
 package kctl
 
 import (
+	"log"
 	"os/exec"
+	"time"
 
 	"github.com/kf5i/k3ai-core/internal/plugins"
 )
@@ -19,12 +21,23 @@ type Wait interface {
 	Process(labels []string)
 }
 
+func pause() {
+	time.Sleep(2 * time.Second)
+}
+
 // Apply adds/updates the plugin in a k3s/k8s cluster
 func Apply(config Config, plugin plugins.PluginSpec, evt Wait) error {
-	err := handleYaml(config, apply, plugin)
-	if err != nil {
-		return err
+	_ = createNameSpace(config, plugin.Namespace)
+	pause()
+	for _, yamlSpec := range plugin.Yaml {
+		err := execute(config, k3sExec, kubectl, apply,
+			decodeType(yamlSpec.Type), yamlSpec.URL, "-n", plugin.Namespace)
+		if err != nil {
+			log.Printf("Error during create: %s\n", err.Error())
+		}
+		pause()
 	}
+
 	if evt != nil {
 		evt.Process(plugin.Labels)
 	}
@@ -33,7 +46,18 @@ func Apply(config Config, plugin plugins.PluginSpec, evt Wait) error {
 
 // Delete removes the plugin from the cluster
 func Delete(config Config, plugin plugins.PluginSpec) error {
-	return handleYaml(config, delete, plugin)
+	for i := len(plugin.Yaml) - 1; i >= 0; i-- {
+		yamlSpec := plugin.Yaml[i]
+		err := execute(config, k3sExec, kubectl, delete,
+			decodeType(yamlSpec.Type), yamlSpec.URL, "-n", plugin.Namespace)
+		if err != nil {
+			log.Printf("Error during delete: %s\n", err.Error())
+
+		}
+		pause()
+	}
+
+	return nil
 }
 
 func decodeType(commandType string) string {
@@ -41,25 +65,6 @@ func decodeType(commandType string) string {
 		return "-k"
 	}
 	return "-f"
-}
-
-func handleYaml(config Config, command string, plugin plugins.PluginSpec) error {
-	for _, yamlSpec := range plugin.Yaml {
-		if command == apply {
-			_ = createNameSpace(config, yamlSpec.NameSpace)
-		}
-		command, args := prepareCommand(config, command,
-			decodeType(yamlSpec.Type), yamlSpec.URL, "-n", yamlSpec.NameSpace)
-		err := execute(config, command, args...)
-		if err != nil {
-			return err
-		}
-		if command == delete {
-			_ = deleteNameSpace(config, yamlSpec.NameSpace)
-		}
-
-	}
-	return nil
 }
 
 func execute(config Config, command string, args ...string) error {

internal/plugins/plugin_spec.go
@@ -16,18 +16,18 @@ const (
 
 // YamlSpec is the specification for Yaml segment of the PluginSpec
 type YamlSpec struct {
-	URL       string `yaml:"url"`
-	NameSpace string `yaml:"namespace,omitempty"`
-	Type      string `yaml:"type,omitempty"`
+	URL  string `yaml:"url"`
+	Type string `yaml:"type,omitempty"`
 }
 
 //PluginSpec is the specification of each k3ai plugin
 type PluginSpec struct {
+	Namespace  string     `yaml:"namespace,omitempty"`
 	Labels     []string   `yaml:",flow"`
 	PluginName string     `yaml:"plugin-name"`
-	Yaml       []YamlSpec `yaml:"yaml"`
-	Bash       []string   `yaml:"bash"`
-	Helm       []string   `yaml:"helm"`
+	Yaml       []YamlSpec `yaml:"yaml,flow"`
+	Bash       []string   `yaml:"bash,flow"`
+	Helm       []string   `yaml:"helm,flow"`
 }
 
 // PluginSpecs is a PluginSpec collection
@@ -45,12 +45,12 @@ func Encode(pluginURI string) (*PluginSpec, error) {
 
 // validate checks for any errors in the PluginSpec
 func (ps *PluginSpec) validate() error {
+	if ps.Namespace == "" {
+		return errors.New("namespace value must be 'default' or another value")
+	}
 	for _, spec := range ps.Yaml {
 		if spec.Type != CommandKustomize && spec.Type != commandFile {
 			return errors.New("type must be file or kustomize")
-		}
-		if spec.NameSpace == "" {
-			return errors.New("namespace value must be 'default' or another value")
 		}
 	}
 	return nil
@@ -59,23 +59,23 @@ func (ps *PluginSpec) validate() error {
 func unmarshal(in []byte) (*PluginSpec, error) {
 	var ps PluginSpec
 	err := yaml.Unmarshal(in, &ps)
-	mergeWithDefault(ps)
+	mergeWithDefault(&ps)
+
 	if err != nil {
 		return nil, err
 	}
 	return &ps, nil
 }
 
-func mergeWithDefault(ps PluginSpec) {
+func mergeWithDefault(ps *PluginSpec) {
+	if ps.Namespace == "" {
+		ps.Namespace = "default"
+	}
 	for i, spec := range ps.Yaml {
-		nameSpace := spec.NameSpace
 		yamlType := spec.Type
-		if spec.NameSpace == "" {
-			nameSpace = "default"
-		}
 		if spec.Type == "" {
 			yamlType = "file"
 		}
-		ps.Yaml[i] = YamlSpec{Type: yamlType, NameSpace: nameSpace, URL: spec.URL}
+		ps.Yaml[i] = YamlSpec{Type: yamlType, URL: spec.URL}
 	}
 }

internal/plugins/plugin_spec_test.go
@@ -9,11 +9,12 @@ import (
 func TestValidate(t *testing.T) {
 	file := getTestSpecFile(t, "defaults/plugin.yaml")
 	testPluginSpec, err := unmarshal(file)
+
 	if err != nil {
 		t.Fatal("failed to unmarshal test file")
 	}
 	var tests = []PluginSpec{
-		PluginSpec{},
+		PluginSpec{Namespace: "default"},
 		*testPluginSpec,
 	}
 	for i, test := range tests {
@@ -33,7 +34,7 @@ func TestValidateDefaultValues(t *testing.T) {
 		t.Fatal("failed to unmarshal test file")
 	}
 	var tests = []PluginSpec{
-		PluginSpec{},
+		PluginSpec{Namespace: "default"},
 		*testPluginSpec,
 	}
 	for i, test := range tests {

internal/plugins/testdata/defaults/plugin.yaml
@@ -1,9 +1,8 @@
 plugin-name: Argo
+namespace: "argo"
 yaml:
   - url: "https://raw.githubusercontent.com/argoproj/argo/stable/manifests/namespace-install.yaml"
-    namespace: "argo"
     type: "file"
-
 bash:
   - bashfile1
   - bashfile2

commit 6a5950e12f700cd07e3a25a0b23f761d1b07ee1a

Fix --kubectl flag This was broken since #37. Fixes #43"

internal/k8s/kctl/deploy.go
@@ -30,8 +30,9 @@ func Apply(config Config, plugin plugins.PluginSpec, evt Wait) error {
 	_ = createNameSpace(config, plugin.Namespace)
 	pause()
 	for _, yamlSpec := range plugin.Yaml {
-		err := execute(config, k3sExec, kubectl, apply,
+		command, args := prepareCommand(config, apply,
 			decodeType(yamlSpec.Type), yamlSpec.URL, "-n", plugin.Namespace)
+		err := execute(config, command, args...)
 		if err != nil {
 			log.Printf("Error during create: %s\n", err.Error())
 		}
@@ -48,8 +49,9 @@ func Apply(config Config, plugin plugins.PluginSpec, evt Wait) error {
 func Delete(config Config, plugin plugins.PluginSpec) error {
 	for i := len(plugin.Yaml) - 1; i >= 0; i-- {
 		yamlSpec := plugin.Yaml[i]
-		err := execute(config, k3sExec, kubectl, delete,
+		command, args := prepareCommand(config, delete,
 			decodeType(yamlSpec.Type), yamlSpec.URL, "-n", plugin.Namespace)
+		err := execute(config, command, args...)
 		if err != nil {
 			log.Printf("Error during delete: %s\n", err.Error())
 