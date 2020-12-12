f5e5afc6a59429dda9ab23c730197c7875cd94ac
Gabriele Santomaggio <g.santomaggio@gmail.com>
2020-12-12 15:32:04 +0100

    lint

--- internal/plugins/cache.go
+++ internal/plugins/cache.go
@@ -2,18 +2,21 @@ package plugins
 
 import "github.com/kf5i/k3ai-core/internal/shared"
 
+// CacheItem the item cache
 type CacheItem struct {
 	Name        string `yaml:"name"`
 	Description string `yaml:"description"`
 }
 
+// Cache for plugins and groups
 type Cache struct {
-	Name  string  `yaml:"name"`
-	Type  string  `yaml:"type"`
+	Name  string      `yaml:"name"`
+	Type  string      `yaml:"type"`
 	Items []CacheItem `yaml:"items,flow"`
 }
 
+// Encode encode the cache to be printed
 func (cache *Cache) Encode(URL string, fileCache string) error {
-	cacheUrl := shared.IncludeSlash(URL, "/") + fileCache
-	return encode(cacheUrl, cache)
+	cacheURL := shared.IncludeSlash(URL, "/") + fileCache
+	return encode(cacheURL, cache)
 }
